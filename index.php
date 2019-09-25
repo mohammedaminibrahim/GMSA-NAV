55cb1a953a241f63b47ef7f81d676650952c4f16
Mohammed Amin Ibrahim <mohammedaminibrahim10@gmail.com>
2019-09-25 20:12:40 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>GMSA - UDS NAVRONGO CAMPUS</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/modern-business.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <?php include 'navbar.php';?>
+
+  
+  
+  
+
+<!-- Image Carousel starts here -->
+<div class="carousel slide" data-ride="carousel" id="imgCarousel" data-interval="3000" data-pause="false">
+
+
+        <!-- Carousel indicators start here -->
+          <ol class="carousel-indicators">
+              <li data-target="#imgCarousel" data-slide-to="0" class="active"></li>
+              <li data-target="#imgCarousel" data-slide-to="1"></li>
+              <li data-target="#imgCarousel" data-slide-to="2"></li>
+          </ol>
+        <!-- Carousel indicators end here -->
+
+          <!-- Container for pictures -->
+          <div class="carousel-inner" role="listbox">
+
+              <div class="carousel-item active">
+                  <img src="img/adventure-alps.jpg" alt="" class="img-fluid d-block" style="width:100%; height:650px;">
+                  <div class="carousel-caption d-none d-md-block">
+                    <div class="container">
+                      <h3 class="display-2">AgroX</h3>
+                      <p class="lead">
+                          We connect farmers to buyers and buyers to farmers all over Ghana.
+                          The lack of an access to a structured market results in smallholder farmers being forced to sell to middlemen who buy at ridiculously exploitative prices. The lack of access to finance also means farmers are never able to move from being smallholders to middle level or even commercial farmers. 
+
+                        </p>
+                        <a href="#main" class="btn btn-lg btn-outline-light">Read more</a>
+                    </div>
+                      
+                  </div>
+              </div>
+
+              <div class="carousel-item">
+                  <img src="img/adventure-alps.jpg" alt="" class="img-fluid d-block" style="width:100%; height: 650px;">
+                  <div class="carousel-caption d-none d-md-block">
+                    <div class="container">
+                      <h3 class="display-2">Mission &amp; Vision</h3>
+                      <p class="lead">
+                          Onboarding smallholder farmers onto the AgroX platform. Agents visit communities where smallholder farmers who deal in sorghum, rice, maize, millet and soybean are registered onto the AgroX platform to trade. Agents also work with farmer based organizations (FBOs) to carry out trading activities.
+                        </p>
+                        <a href="#main" class="btn btn-lg btn-outline-light">Read More</a>
+                    </div>
+                      
+                  </div>
+              </div>
+
+              <div class="carousel-item">
+                  <img src="img/adventure-alps.jpg" alt="" class="img-fluid d-block" style="width:100%; height: 650px;">
+                  <div class="carousel-caption d-none d-md-block">
+                    <div class="container">
+                      <h3 class="display-2">Meet Farmers</h3>
+                      <p class="lead">
+                          Gathering market information and statistical data. Agents are assigned to major trading markets across the country to collate data on market pricing for various commodities. This information is relayed to smallholder farmers via Voice technologies in languages they read and understand. 
+
+                        </p>
+                        <a href="#main" class="btn btn-lg btn-outline-light">Read More</a>
+                    </div>
+                      
+                  </div>
+              </div>
+              
+
+          </div>
+
+          <!-- Carousel controls start here -->
+          <a href="#imgCarousel" class="carousel-control-prev" data-slide="prev">
+              <span class="carousel-control-prev-icon"></span>
+          </a>
+          <a href="#imgCarousel" class="carousel-control-next" data-slide="next">
+              <span class="carousel-control-next-icon"></span>
+          </a>
+          <!-- Carousel controls end here -->
+
+
+  </div>
+
+
+
+
+
+
+
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <h1 class="my-4">Welcome to Modern Business</h1>
+
+    <!-- Marketing Icons Section -->
+    <div class="row">
+      <div class="col-lg-4 mb-4">
+        <div class="card h-100">
+          <h4 class="card-header">Card Title</h4>
+          <div class="card-body">
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Learn More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 mb-4">
+        <div class="card h-100">
+          <h4 class="card-header">Card Title</h4>
+          <div class="card-body">
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ipsam eos, nam perspiciatis natus commodi similique totam consectetur praesentium molestiae atque exercitationem ut consequuntur, sed eveniet, magni nostrum sint fuga.</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Learn More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 mb-4">
+        <div class="card h-100">
+          <h4 class="card-header">Card Title</h4>
+          <div class="card-body">
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Learn More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /.row -->
+
+    <!-- Portfolio Section -->
+    <h2>Portfolio Heading</h2>
+
+    <div class="row">
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <a href="#">Project One</a>
+            </h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <a href="#">Project Two</a>
+            </h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <a href="#">Project Three</a>
+            </h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos quisquam, error quod sed cumque, odio distinctio velit nostrum temporibus necessitatibus et facere atque iure perspiciatis mollitia recusandae vero vel quam!</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <a href="#">Project Four</a>
+            </h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <a href="#">Project Five</a>
+            </h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <a href="#">Project Six</a>
+            </h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum suscipit ducimus nihil provident, perferendis rem illo, voluptate atque, sit eius in voluptates, nemo repellat fugiat excepturi! Nemo, esse.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /.row -->
+
+    <!-- Features Section -->
+    <div class="row">
+      <div class="col-lg-6">
+        <h2>Modern Business Features</h2>
+        <p>The Modern Business template by Start Bootstrap includes:</p>
+        <ul>
+          <li>
+            <strong>Bootstrap v4</strong>
+          </li>
+          <li>jQuery</li>
+          <li>Font Awesome</li>
+          <li>Working contact form with validation</li>
+          <li>Unstyled page elements for easy customization</li>
+        </ul>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, omnis doloremque non cum id reprehenderit, quisquam totam aspernatur tempora minima unde aliquid ea culpa sunt. Reiciendis quia dolorum ducimus unde.</p>
+      </div>
+      <div class="col-lg-6">
+        <img class="img-fluid rounded" src="http://placehold.it/700x450" alt="">
+      </div>
+    </div>
+    <!-- /.row -->
+
+    <hr>
+
+    <!-- Call to Action Section -->
+    <div class="row mb-4">
+      <div class="col-md-8">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, expedita, saepe, vero rerum deleniti beatae veniam harum neque nemo praesentium cum alias asperiores commodi.</p>
+      </div>
+      <div class="col-md-4">
+        <a class="btn btn-lg btn-secondary btn-block" href="#">Call to Action</a>
+      </div>
+    </div>
+
+  </div>
+  <!-- /.container -->
+
+ <!-- Footer -->
+        <?php
+            include 'footer.php';
+        
+        ?>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
